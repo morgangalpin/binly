6b5042c8fda128f07c9deac73cba2207d0e39dac
Morgan Galpin <mgalpin@salesforce.com>
2017-11-01 22:34:51 -0700

    Adjusted smoothing parameters and cleaned up.

--- binly/platform/resources/servo.py
+++ binly/platform/resources/servo.py
@@ -15,10 +15,10 @@ class Servo(Resource):
     DEFAULT_SERVO_MAX = 545
 
     # Max number of servo units to change per time unit.
-    DEFAULT_MAX_SERVO_RATE = 25
+    DEFAULT_MAX_SERVO_RATE = 10
 
     # Number of seconds between servo movement updates.
-    SERVO_UPDATE_SLEEP_SECONDS = 1.0 / 10.0
+    SERVO_UPDATE_SLEEP_SECONDS = 1.0 / 30.0
 
     # Servo pulse width modulation frequency in Hertz.
     SERVO_PWM_FREQ_HZ = 50
@@ -49,12 +49,9 @@ class Servo(Resource):
             self._pwm.set_pwm_freq(self.SERVO_PWM_FREQ_HZ)
 
         # Initialize the servo to the initial value.
-        self.servo_value = self.scale_value(
+        self.set_servo_value(self.scale_value(
             initial_value, self.min_value, self.max_value,
-            self.servo_min, self.servo_max)
-        logging.debug('Setting servo value to initial value: %s'
-                      % (self.servo_value))
-        self._pwm.set_pwm(self.servo_channel, 0, self.servo_value)
+            self.servo_min, self.servo_max))
 
     def start(self):
         self.start_processing_incoming_messages()
@@ -75,48 +72,19 @@ class Servo(Resource):
         for servo_value in self.smooth_value_series(self.servo_value,
                                                     new_servo_value,
                                                     self.max_servo_rate):
-            logging.debug('Setting servo value to: %s' % (servo_value))
-            self._pwm.set_pwm(self.servo_channel, 0, servo_value)
+            self.set_servo_value(int(servo_value))
             time.sleep(self.SERVO_UPDATE_SLEEP_SECONDS)
-        self.servo_value = new_servo_value
+
+    def set_servo_value(self, value):
+        logging.debug('Setting servo value to: %s' % (value))
+        self._pwm.set_pwm(self.servo_channel, 0, value)
+        self.servo_value = value
 
     @staticmethod
     def smooth_value_series(start_value, end_value, max_rate):
         num_samples = int(
             math.ceil(abs(float(start_value - end_value)) / max_rate)) + 1
         return numpy.linspace(start_value, end_value, num_samples)
-        # if length <= 1:
-        #     return [end_value]
-        # step_size = float(start_value - end_value) / (length - 1)
-        # result = []
-
-        # return result
-
-    #     # create function for doing interpolation of the desired
-    #     # ranges
-    #     scaler = make_interpolater(1, 512, 5, 10)
-
-    #     # receive list of raw values from sensor, assign to data_list
-
-    #     # now convert to scaled values using map
-    #     scaled_data = map(scaler, data_list)
-
-    #     # or a list comprehension, if you prefer
-    #     scaled_data = [scaler(x) for x in data_list]
-
-    # def make_interpolater(left_min, left_max, right_min, right_max):
-    #     # Figure out how 'wide' each range is
-    #     leftSpan = left_max - left_min
-    #     rightSpan = right_max - right_min
-
-    #     # Compute the scale factor between left and right values
-    #     scaleFactor = float(rightSpan) / float(leftSpan)
-
-    #     # create interpolation function using pre-calculated scaleFactor
-    #     def interp_fn(value):
-    #         return right_min + (value - left_min) * scaleFactor
-
-    #     return interp_fn
 
 
 class FakePCA9685(object):
